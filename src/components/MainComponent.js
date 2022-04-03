fa49eddd73840d1c26278bc16ec174f55fb7adb6
Scheibelr <scheibelr@gmail.com>
2022-04-02 22:30:07 -0400

    button to randomize rolls

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,23 +7,31 @@ function Main() {
         return (Math.floor(Math.random() * max))+1;
         }
         console.log()
+
+    const [diceArray, setDiceArray] = React.useState([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i <= 9; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return (
+            newDice,
+            setDiceArray(newDice)
+        )
+    }
+
+    const diceElements = diceArray.map(die => <Die value={die} />)
+
     return (
         <>
         <div className="container">
             <div className="main">
                 <div className="dice-container">
-                    <Die value={value}/>
-                    <Die value={value}/>
-                    <Die value={value}/>
-                    <Die value={value}/>
-                    <Die value={value}/>
-                    <Die value={value}/>
-                    <Die value={value}/>
-                    <Die value={value}/>
-                    <Die value={value}/>
-                    <Die value={value}/>
+                    {diceElements}
                 </div>
-
+            <button className="roll" onClick={allNewDice}>Roll</button>
             </div>
         </div>
         </>
